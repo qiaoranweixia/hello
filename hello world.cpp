e82ab6a0325f7d47781c994527f3673bdbd46e9f
qiaoranweixia <101105463+qiaoranweixia@users.noreply.github.com>
2026-03-03 18:53:58 +0800

    CDS

---
+++ hello world.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+std::array<int, 4> coins = { 64, 16, 4, 1 };
+
+int coinChange(int rest, int idx) {
+    if (rest > coins[idx])  
+    {
+        rest -= coins[idx];
+        return 1 + coinChange(rest, idx);
+    }
+    else if (rest > 0)
+    {
+        return coinChange(rest, ++idx);
+    }
+    return 1;  
+}
+int main() {
+    int n;
+    cin >> n;
+    int ans = coinChange(1024 - n, 0);
+    cout << ans << endl;
+    return 0;
+}
